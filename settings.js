1ab34d8e704e7c26615808d41580fd24c5d9ba81
Dilisha-LK <dilishagimshan95@gmail.com>
2024-11-22 19:53:54 +0530

    🎲 Zeus-MD 🎲

---
+++ settings.js
@@ -0,0 +1,49 @@
+/* Bot settings 
+
+You don't have to set this if you deploy using heroku because you can simply set them in environment variables, also don't forget to sleep */
+
+// srilanka super md in made by dilisha gimshan.
+
+const session = process.env.SESSION || ' put your session id';
+
+const prefix = process.env.PREFIX || '.';
+const mycode = process.env.CODE || "263";
+const author = process.env.STICKER_AUTHOR || 'Dilisha';
+const packname = process.env.PACKNAME || 'dilisha';
+const dev = process.env.DEV || '263714757857';
+const DevDreaded = dev.split(",");
+const botname = process.env.BOTNAME || 'Zeus-MD';
+const mode = process.env.MODE || 'public';
+const gcpresence = process.env.GC_PRESENCE || 'false';
+const antionce = process.env.ANTIVIEWONCE || 'true';
+const sessionName = "session";
+const presence = process.env.WA_PRESENCE || 'online';
+
+const antitag = process.env.ANTITAG || 'true';
+const antidelete = process.env.ANTIDELETE || 'true';
+const autoview = process.env.AUTOVIEW_STATUS || 'true';
+const autolike = process.env.AUTOLIKE_STATUS || 'true';
+const autoread = process.env.AUTOREAD || 'true';
+const autobio = process.env.AUTOBIO || 'false';
+
+module.exports = {
+  sessionName,
+  presence,
+  autoview,
+  autoread,
+  botname,
+  autobio,
+  mode,
+  prefix,
+  autolike,
+  mycode,
+  author,
+  packname,
+  dev,
+  DevDreaded,
+  gcpresence,
+  antionce,
+session,
+antitag,
+antidelete
+};
