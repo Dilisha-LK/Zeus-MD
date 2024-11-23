9599099e40b2a0b6440918e3e03bb1b8ab44d72a
Dilisha-LK <dilishagimshan95@gmail.com>
2024-11-23 18:22:50 +0530

    🎲 Zeus-MD 🎲

--- Cmds/General/ping.js
+++ Cmds/General/ping.js
@@ -2,6 +2,6 @@ module.exports = async (context) => {
         const { client, m, dreadedspeed } = context;
 
 
-await m.reply(`Zeus-MD .█▇▆▅▄▂ speed=: \n${dreadedspeed.toFixed(4)}𝐌\𝐒`)
+await m.reply(`Zeus-MD's█▇▆▅▄▂ speed=: \n${dreadedspeed.toFixed(4)}𝐌\𝐒`)
 
 }
